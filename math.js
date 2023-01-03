3c8127123a886a5fe266172fc18f0b672b98d8a0
mickotomic <milorad.tomic.87@gmail.com>
2023-01-03 15:36:41 +0100

    Update math.js

--- math.js
+++ math.js
@@ -1,10 +1,21 @@
 import axios from "axios";
 
+const expression = "4 * (a5 - 3)";
+
+const URL = `http://api.mathjs.org/v4/?expr=${expression}`;
+
 async function math() { 
     try {
+        const response = await axios.get(URL);
+        console.log(`Status: SUCCESS\nValue: ${response.data}\nExpression: ${expression}\n`);
         
     }
-    catch (e) {
-        console.log(e);
+    catch (err) {
+        console.log(`Status: FAILURE\nValue: N/A\nExpression: ${expression}\nReason: ` + err);
+       
     }
+}
+
+if ((/^[0-9*-+/()]/g).test(expression)) { 
+    math();
 }
